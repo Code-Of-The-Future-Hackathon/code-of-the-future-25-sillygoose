d607b447e2bb809491a473489fdf2cd4806b46af
somebody241 <116384112+somebody241@users.noreply.github.com>
2025-02-23 09:18:18 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,197 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Net;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        public static bool Query(string key, float value, string query, MySqlConnection conn, string log_measure)
+        {
+            string updated_query = query.Replace("@key", key);
+
+            using MySqlCommand cmd = new MySqlCommand(updated_query, conn);
+            cmd.Parameters.AddWithValue("@RAM_Used_Percent", value);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Console.WriteLine($"{key} Exceeded {value:0.00}{log_measure}");
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            float w_ram_percent_used = 80;
+            float w_cpu_usage = 20;
+            float w_storage_percent_used = 80;
+            float w_wifi_up_usage = 50;
+            float w_wifi_down_usage = 50;
+            float w_ethernet_up_usage = 50;
+            float w_ethernet_down_usage = 50;
+
+            DateTime next_ram_warning = DateTime.Now;
+            DateTime next_cpu_usage_warning = DateTime.Now;
+            DateTime next_storage_warning = DateTime.Now;
+            DateTime next_wifi_warning = DateTime.Now;
+            DateTime next_ethernet_warning = DateTime.Now;
+
+            int minutes_cd_ram_warning = 1;
+            int minutes_cd_cpu_usage_warning = 1;
+            int minutes_cd_storage_warning = 1;
+            int minutes_cd_wifi_warning = 1;
+            int minutes_cd_ethernet_warning = 1;
+
+
+            var fromAddress = new MailAddress("sender@gmail.com", "Sender");
+            var toAddress = new MailAddress("receiver@gmail.com", "Receiver");
+            const string fromPassword = "password";
+
+
+            string connectionString = "server=localhost;user=root;password=mamati1;database=Sensors";
+
+            using MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            string query = "SELECT Time_Of_Reading, @key FROM SensorReadings " +
+                "WHERE @key >= @RAM_Used_Percent AND TIMESTAMPDIFF(SECOND, Time_Of_Reading, NOW()) <= 3";
+
+            string log_query = "INSERT INTO WarningLogs(Time_Of_Logging, Warning_Type, Warning_Message) " +
+                "VALUES (@time, @type, @message)";
+
+            while(true)
+            {
+                if(Query("RAM_Used_Percent", w_ram_percent_used, query, conn, "%"))
+                {
+                    if(next_ram_warning<=DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "RAM Usage");
+                        cmd.Parameters.AddWithValue("@message", $"RAM usage exceeds the specified {w_ram_percent_used}%");
+
+                        cmd.ExecuteNonQuery();
+
+                        //const string subject = "Warning: RAM usage exceeds the specified limit";
+                        //const string body = "";
+
+                        //var smtp = new SmtpClient
+                        //{
+                        //    Host = "smtp.gmail.com", // e.g., smtp.gmail.com for Gmail
+                        //    Port = 587, // Use 465 for SSL or 587 for TLS
+                        //    EnableSsl = false,
+                        //    DeliveryMethod = SmtpDeliveryMethod.Network,
+                        //    UseDefaultCredentials = false,
+                        //    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                        //};
+
+                        Console.WriteLine("---");
+                        using (var message = new MailMessage(fromAddress, toAddress)
+                        {
+                            Subject = subject,
+                            Body = body
+                        })
+                        {
+                            smtp.Send(message);
+                        }
+                        Console.WriteLine("----");
+
+                        next_ram_warning = DateTime.Now.AddMinutes(minutes_cd_ram_warning);
+                    }
+                }
+                if(Query("CPU_Usage", w_cpu_usage, query, conn, "%"))
+                {
+                    if (next_cpu_usage_warning <= DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "CPU Usage");
+                        cmd.Parameters.AddWithValue("@message", $"CPU usage exceeds the specified {w_cpu_usage}%");
+
+                        cmd.ExecuteNonQuery();
+
+                        next_cpu_usage_warning = DateTime.Now.AddMinutes(minutes_cd_cpu_usage_warning);
+                    }
+                }
+                if(Query("Storage_Used_Percent", w_storage_percent_used, query, conn, "%"))
+                {
+                    if (next_storage_warning <= DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "Storage Low");
+                        cmd.Parameters.AddWithValue("@message", $"Storage used exceeds the specified {w_storage_percent_used}%");
+
+                        cmd.ExecuteNonQuery();
+
+                        next_storage_warning = DateTime.Now.AddMinutes(minutes_cd_storage_warning);
+                    }
+                }
+                if(Query("Wifi_Up", w_wifi_up_usage, query, conn, ""))
+                {
+                    if (next_wifi_warning <= DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "Wifi Usage");
+                        cmd.Parameters.AddWithValue("@message", $"Wifi usage exceeds the specified {w_wifi_up_usage}");
+
+                        cmd.ExecuteNonQuery();
+
+                        next_wifi_warning = DateTime.Now.AddMinutes(minutes_cd_wifi_warning);
+                    }
+                }
+                if(Query("Wifi_Down", w_wifi_down_usage, query, conn, ""))
+                {
+                    if (next_wifi_warning <= DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "Wifi Usage");
+                        cmd.Parameters.AddWithValue("@message", $"Wifi usage exceeds the specified {w_wifi_down_usage}");
+
+                        cmd.ExecuteNonQuery();
+
+                        next_wifi_warning = DateTime.Now.AddMinutes(minutes_cd_wifi_warning);
+                    }
+                }
+                if(Query("Ethernet_Up", w_ethernet_up_usage, query, conn, ""))
+                {
+                    if (next_ethernet_warning <= DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "Ethernet Usage");
+                        cmd.Parameters.AddWithValue("@message", $"Ethernet usage exceeds the specified {w_ethernet_up_usage}");
+
+                        cmd.ExecuteNonQuery();
+
+                        next_ethernet_warning = DateTime.Now.AddMinutes(minutes_cd_ethernet_warning);
+                    }
+                }
+                if(Query("Ethernet_Down", w_ethernet_down_usage, query, conn, ""))
+                {
+                    if (next_ethernet_warning <= DateTime.Now)
+                    {
+                        using MySqlCommand cmd = new MySqlCommand(log_query, conn);
+                        cmd.Parameters.AddWithValue("@time", DateTime.Now);
+                        cmd.Parameters.AddWithValue("@type", "Ethernet Usage");
+                        cmd.Parameters.AddWithValue("@message", $"Ethernet usage exceeds the specified {w_ethernet_down_usage}");
+
+                        cmd.ExecuteNonQuery();
+
+                        next_ethernet_warning = DateTime.Now.AddMinutes(minutes_cd_ethernet_warning);
+                    }
+                }
+
+                Console.WriteLine();
+
+                Thread.Sleep(1000);
+            }
+
+            conn.Close();
+        }
+    }
+}
---
+++ DataCollection.cs
@@ -0,0 +1,126 @@
+using System;
+using LibreHardwareMonitor.Hardware;
+using MySql.Data.MySqlClient;
+
+public class DataCollection
+{
+    static void Main()
+    {
+        Computer computer = new Computer
+        {
+            IsCpuEnabled = true,
+            IsStorageEnabled = true,
+            IsMemoryEnabled = true,
+            IsNetworkEnabled = true
+        };
+
+        computer.Open();
+
+        float? core1_temp=0, core1_dist_max=0;
+        float? cpu_max_temp = 0;
+        float? cpu_avg_temp = 0;
+        float? cpu_usage = 0;
+
+        float? used_memory_percent = 0;
+
+        float? used_storage_percent = 0;
+
+        float? wifi_up = 0, wifi_down = 0;
+        float? ethernet_up = 0, ethernet_down = 0;
+
+        string connectionString = "server=localhost;user=root;password=mamati1;database=Sensors";
+
+        using MySqlConnection conn = new MySqlConnection(connectionString);
+        conn.Open();
+
+        for(int i = 0; i<10; ++i)
+        {
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+                //Console.WriteLine($"Hardware: {hardware.Name} ({hardware.HardwareType})");
+
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    if (sensor.Name == "Memory")
+                    {
+                        used_memory_percent = sensor.Value;
+                    }
+                    if (sensor.Name == "Used Space")
+                    {
+                        used_storage_percent = sensor.Value;
+                    }
+                    if (sensor.SensorType == SensorType.Temperature && sensor.Name == "CPU Core #1")
+                    {
+                        core1_temp = sensor.Value;
+                    }
+                    if (sensor.SensorType == SensorType.Temperature && sensor.Name == "CPU Core #1 Distance to TjMax")
+                    {
+                        core1_dist_max = sensor.Value;
+                    }
+                    if (sensor.SensorType == SensorType.Temperature && sensor.Name == "Core Average")
+                    {
+                        cpu_avg_temp = sensor.Value;
+                    }
+                    if (sensor.Name == "CPU Total")
+                    {
+                        cpu_usage = sensor.Value;
+                    }
+                    if (hardware.Name.Contains("WiFi") && sensor.Name == "Data Uploaded")
+                    {
+                        wifi_up = sensor.Value;
+                    }
+                    if (hardware.Name.Contains("WiFi") && sensor.Name == "Data Downloaded")
+                    {
+                        wifi_down = sensor.Value;
+                    }
+                    if (hardware.Name.Contains("Ethernet") && sensor.Name == "Data Uploaded")
+                    {
+                        ethernet_up = sensor.Value;
+                    }
+                    if (hardware.Name.Contains("Ethernet") && sensor.Name == "Data Downloaded")
+                    {
+                        ethernet_down = sensor.Value;
+                    }
+
+                    cpu_max_temp = core1_temp + core1_dist_max;
+
+
+
+                }
+
+            }
+
+            string query = "INSERT INTO SensorReadings (Time_Of_Reading, RAM_Used_Percent, CPU_Max_Temp, CPU_Temp, CPU_Usage, " +
+                        "Storage_Used_Percent, Wifi_Up, Wifi_Down, Ethernet_Up, Ethernet_Down) " +
+                        "VALUES (@Time_Of_Reading, @RAM_Used_Percent, @CPU_Max_Temp, @CPU_Temp, @CPU_Usage, @Storage_Used_Percent, " +
+                        "@Wifi_Up, @Wifi_Down, @Ethernet_Up, @Ethernet_Down)";
+
+            using MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Time_Of_Reading", DateTime.Now);
+            cmd.Parameters.AddWithValue("@RAM_Used_Percent", used_memory_percent);
+            cmd.Parameters.AddWithValue("@CPU_Max_Temp", cpu_max_temp);
+            cmd.Parameters.AddWithValue("@CPU_Temp", cpu_avg_temp);
+            cmd.Parameters.AddWithValue("@CPU_Usage", cpu_usage);
+            cmd.Parameters.AddWithValue("@Storage_Used_Percent", used_storage_percent);
+            cmd.Parameters.AddWithValue("@Wifi_Up", wifi_up);
+            cmd.Parameters.AddWithValue("@Wifi_Down", wifi_down);
+            cmd.Parameters.AddWithValue("@Ethernet_Up", ethernet_up);
+            cmd.Parameters.AddWithValue("@Ethernet_Down", ethernet_down);
+
+            int rowsAffected = cmd.ExecuteNonQuery();
+            Console.WriteLine(rowsAffected);
+
+            Thread.Sleep(2000);
+        }
+        
+
+        Console.WriteLine();
+
+        computer.Close();
+        conn.Close();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
